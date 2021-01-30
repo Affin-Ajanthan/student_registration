1bcb968d0e64a605aedf69a2aa92aa98bf6f5336
Affin Ajanthan <59964903+Affin-Ajanthan@users.noreply.github.com>
2021-01-30 18:31:08 +0530

    Add files via upload

---
+++ Database/dbo.reguser.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[reguser] (
+    [regno]    VARCHAR (20) NULL,
+    [name]     VARCHAR (50) NULL,
+    [mobile]   BIGINT          NULL,
+    [email]    VARCHAR (50) NULL,
+    [username] VARCHAR (50) NULL,
+    [password] VARCHAR (50) NULL
+);
+
